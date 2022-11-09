a986762fa53225985fc104349eae03b6d232ce00
alex.kim <alkkim@gmail.com>
2022-11-08 23:06:33 -0800

    ask user for input for password requirement and confirm choices after each question

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // Assignment code here
 
+function generatePassword() {
+  var pwdLength = window.prompt("Choose the length of your password. Number must be between 8 and 128.");
+  alert("You entered " + pwdLength);
+  var lowerCase = window.prompt("Choose Y or N to include lowercase letters");
+  alert("You entered " + lowerCase + " for lower case letters.");
+  var upperCase = window.prompt("Choose Y or N to include uppercase letters");
+  alert("You entered " + upperCase + " for upper case letters.");
+  var specialChar = window.prompt("Choose Y or N to include special characters");
+  alert("You entered " + specialChar + " for special characters.");
+  var numbers = window.prompt("Choose Y or N to include numbers (0-9)");
+  alert("You entered " + numbers + " for numbers.");
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
